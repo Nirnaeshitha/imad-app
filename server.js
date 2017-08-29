9296c846482a075d42bbd5923b7532c7ae18d015
Nirnaeshitha <30804914+Nirnaeshitha@users.noreply.github.com>
2017-08-29 17:08:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one',function(req,res)
 {
-     res.send(createTemplate(aticleOne);
+     res.send(createTemplate(aticleOne));
 });
 
 app.get('/article-two',function(req,res)
