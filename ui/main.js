588722574959f49930f76deddf68dcda79826e1f
Nirnaeshitha <30804914+Nirnaeshitha@users.noreply.github.com>
2017-08-30 19:57:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,9 +13,14 @@ button.onclick = function() {
       {
           if(request.status == 200)
           {
-              var counter = request.responseText;
-              var span = document.getElementById('count');
-              span.innerHTML = counter.toString();
+             var names = ['name1','name2','name3','name4'];
+             var list = '';
+             for( var i=0 ; i<=names.length ; i++)
+                {
+                list <- '<li>' + names[i] + '</li>';
+                }
+             var ul = document.getElementById("namelist");
+             ul.innerHTML = list ;
           }
      }  
     };
@@ -28,7 +33,7 @@ var name = nameInput.value;
 var submit = getElemntById("submit_btn");
 submit.onclick =  function()
 {
-   var names = ['name1','name2','name3'];
+   var names = ['name1','name2','name3','name4'];
    var list = '';
    for( var i=0 ; i<=names.length ; i++)
    {
