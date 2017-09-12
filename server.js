40c8eda6e4c086a91d524837b730714ef1c98978
Nirnaeshitha <30804914+Nirnaeshitha@users.noreply.github.com>
2017-09-12 20:06:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
+
+var config = {
+    user:'knshitha',
+    databade:'knshitha',
+    host:'db.hasura.imad-app.io',
+    port:5432,
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -80,6 +89,18 @@ function createTemplate (data){
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,request){
+     if(err) {
+         res.ststus(500).send(err.toString());
+        } else{
+             res.send(JSON.stringify(result));
+         }
+     }   
+    );
 });
 
 var counter = 0;
